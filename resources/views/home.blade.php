4fc1f3bfdf0250f48e56b1c3955f5bc2ba3cb00d
DaniloSalerno <salerno.danilo@hotmail.it>
2023-10-25 15:30:28 +0200

    fix jumbotron

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,6 @@
 @include('partials/head')
 <main>
-    <div class="p-5 mb-4 bg-light rounded-3">
+    <div class="container p-5 mb-4">
         <div class="container-fluid py-5">
 
             <h1 class="display-5 fw-bold">
--- resources/views/partials/jumbo.blade.php
+++ resources/views/partials/jumbo.blade.php
@@ -1,12 +1,14 @@
-<div class="p-5 mb-4 bg-light rounded-3">
-    <div class="container-fluid py-5">
+<div class="container">
+    <div class="p-5 mb-4">
+        <div class="container-fluid py-5">
 
-        <h1 class="display-5 fw-bold">
-            {{$title}}
-        </h1>
+            <h1 class="display-5 fw-bold">
+                {{$title}}
+            </h1>
 
-        <p class="col-md-8 fs-4">
-            {{$message}}
-        </p>
+            <p class="col-md-8 fs-4">
+                {{$message}}
+            </p>
+        </div>
     </div>
 </div>
