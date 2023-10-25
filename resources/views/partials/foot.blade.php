779f7278880ebed807d1cf93f36d957df7f42c23
DaniloSalerno <salerno.danilo@hotmail.it>
2023-10-25 15:21:52 +0200

    add partials fot split the project

---
+++ resources/views/partials/foot.blade.php
@@ -0,0 +1,53 @@
+<footer class="py-5 bg-dark text-white">
+    <div class="container">
+        <div class="row row-cols-1 row-cols-lg-3">
+            <div class="col">
+                <h5>Links</h5>
+                <ul class="list-unstyled">
+                    <li><a class="text-white text-decoration-none" href="#">Lorem.</a></li>
+                    <li><a class="text-white text-decoration-none" href="#">Itaque?</a></li>
+                    <li><a class="text-white text-decoration-none" href="#">Sed.</a></li>
+                    <li><a class="text-white text-decoration-none" href="#">Exercitationem.</a></li>
+                    <li><a class="text-white text-decoration-none" href="#">Repellat.</a></li>
+                    <li><a class="text-white text-decoration-none" href="#">Provident.</a></li>
+                </ul>
+            </div>
+            <!-- /.col -->
+
+            <div class="col">
+                <h5>About</h5>
+                <ul class="list-unstyled">
+                    <li><a class="text-white text-decoration-none" href="#">Lorem.</a></li>
+                    <li><a class="text-white text-decoration-none" href="#">Itaque?</a></li>
+                    <li><a class="text-white text-decoration-none" href="#">Sed.</a></li>
+                    <li><a class="text-white text-decoration-none" href="#">Exercitationem.</a></li>
+                    <li><a class="text-white text-decoration-none" href="#">Repellat.</a></li>
+                    <li><a class="text-white text-decoration-none" href="#">Provident.</a></li>
+                </ul>
+            </div>
+            <!-- /.col -->
+
+            <div class="col">
+                <h5>Contacts</h5>
+                <ul class="list-unstyled">
+                    <li><a class="text-white text-decoration-none" href="#">Lorem.</a></li>
+                    <li><a class="text-white text-decoration-none" href="#">Itaque?</a></li>
+                    <li><a class="text-white text-decoration-none" href="#">Sed.</a></li>
+                    <li><a class="text-white text-decoration-none" href="#">Exercitationem.</a></li>
+                    <li><a class="text-white text-decoration-none" href="#">Repellat.</a></li>
+                    <li><a class="text-white text-decoration-none" href="#">Provident.</a></li>
+                </ul>
+            </div>
+            <!-- /.col -->
+
+        </div>
+        <!-- /.row -->
+
+    </div>
+    <!-- /.container -->
+
+</footer>
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -31,5 +31,10 @@ Route::get('/about', function () {
 });
 
 Route::get('/contacts', function () {
-    return view('contacts');
+    $data = [
+        'message' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis mollitia libero assumenda et distinctio, sit autem praesentium voluptate iure eius voluptatibus nisi dignissimos quisquam iusto molestiae. Laudantium ducimus enim quibusdam quas. Voluptas nisi quisquam reiciendis quidem iure, autem, est quasi non dolorem odit odio voluptate magni placeat minima dicta qui repellendus nemo ut beatae nihil laboriosam, necessitatibus dolores ea. Dolore iste obcaecati deserunt amet, aut animi dolorem quam. Minima, ducimus ad. Totam delectus quaerat unde laborum illo sint doloremque. Minima architecto labore iure libero autem hic! Magnam, amet. Consequuntur rem hic vero natus at quisquam voluptatibus ipsam quod sint sequi autem, nulla, amet ea aut reiciendis optio voluptates ut illo eius dicta distinctio culpa adipisci cumque! Quasi natus distinctio est nihil. Reprehenderit voluptatibus placeat obcaecati! Dolor repudiandae dolorum inventore ex numquam ullam consequuntur, corporis natus veritatis. Ratione voluptatem quaerat doloremque saepe non odit modi, distinctio enim nobis ipsam harum vero repudiandae, ex reprehenderit suscipit labore, aperiam aliquam impedit inventore id ipsum. Magnam esse ipsa iusto, facere quaerat aliquam! Deserunt quidem vel quia dolor asperiores consequuntur ratione fugiat accusantium labore aspernatur, velit vitae veritatis pariatur iure cumque odit sequi! Quod excepturi rem doloremque ipsum beatae porro. Sit eum perspiciatis in repellendus?',
+
+        'title' => 'Contacts Page'
+    ];
+    return view('contacts', $data);
 });
